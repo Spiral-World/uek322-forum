21b153491f804b3d026bd6cd395349fc118329f6
Loadigjablos <loadingjablowme@gmail.com>
2023-04-27 18:56:20 +0200

    added all necacary DB Tables that will get made on start of the application

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,6 +1,6 @@
 import mariadb from 'mariadb'
 import { Pool } from 'mariadb'
-import { USER_TABLE, TWEET_TABLE } from './schema'
+import { USER_TABLE, POSTS_TABLE, LIKES_TABLE, COMMENTS_TABLE } from './schema'
 
 export class Database {
   // Properties
@@ -20,7 +20,9 @@ export class Database {
   private initializeDBSchema = async () => {
     console.log('Initializing DB schema...')
     await this.executeSQL(USER_TABLE)
-    await this.executeSQL(TWEET_TABLE)
+    await this.executeSQL(POSTS_TABLE)
+    await this.executeSQL(LIKES_TABLE)
+    await this.executeSQL(COMMENTS_TABLE)
   }
 
   public executeSQL = async (query: string) => {
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,19 +1,48 @@
 const USER_TABLE = `
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-`
-
-const TWEET_TABLE = `
-CREATE TABLE IF NOT EXISTS tweets (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    content VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    passwdhash VARCHAR(255) NOT NULL,
+    role VARCHAR(8) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    UNIQUE (email)
 );
 `
 
-export { USER_TABLE, TWEET_TABLE }
+const POSTS_TABLE = `
+CREATE TABLE IF NOT EXISTS posts (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    userid INT(11) NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id)
+);
+`
+
+const LIKES_TABLE = `
+CREATE TABLE IF NOT EXISTS likes (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    userid INT(11) NOT NULL,
+    postid INT(11) NOT NULL,
+    likeit int(1) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id),
+    FOREIGN KEY (postid) REFERENCES posts(id)
+);
+`
+
+const COMMENTS_TABLE = `
+CREATE TABLE IF NOT EXISTS comments (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    userid INT(11) NOT NULL,
+    postid INT(11) NOT NULL,
+    text TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id),
+    FOREIGN KEY (postid) REFERENCES posts(id)
+);
+`
+
+export { USER_TABLE, POSTS_TABLE, LIKES_TABLE, COMMENTS_TABLE }
